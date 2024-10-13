dd42cab37acc77200528f3a698a66cb37ffec23d
LisaLowagie <lowagielisa@gmail.com>
2024-10-13 15:24:16 +0200

    Added js for the city

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  //Brussels
+  let brusselsElement = document.querySelector("#brussels");
+  let brusselsDateElement = brusselsElement.querySelector(".date");
+  let brusselsTimeElement = brusselsElement.querySelector(".time");
+  let brusselsTime = moment().tz("Europe/Brussels");
+  brusselsDateElement.innerHTML = brusselsTime.format("MMMM Do YYYY");
+  brusselsTimeElement.innerHTML = `${brusselsTime.format(
+    `h:mm:ss`
+  )} <small>${brusselsTime.format("A")}</small>`;
 
+  //Paris
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format(
+    `h:mm:ss`
+  )} <small>${tokyoTime.format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
